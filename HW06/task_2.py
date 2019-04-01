5a08be9c3d68300dfd60784d67ac87b5e9672710
JuliaBelskaya <48747460+JuliaBelskaya@users.noreply.github.com>
2019-04-01 23:51:10 +0300

    Update task_2.py

---
+++ HW06/task_2.py
@@ -0,0 +1,10 @@
+n = input('Введите двухзначное число ')
+mult = 1
+summa = 0
+for i in n:
+    summa += int(i)
+    if int(i) != 0:
+        mult *= int(i)
+
+print('Сумма цифр:', summa)
+print('Произведение цифр:', mult)
