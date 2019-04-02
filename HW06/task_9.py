262a39525ac51acc363cccc2302eb9852157d0bd
JuliaBelskaya <48747460+JuliaBelskaya@users.noreply.github.com>
2019-04-02 16:07:29 +0300

    Update task_9.py

---
+++ HW06/task_9.py
@@ -0,0 +1,28 @@
+trains = [
+  {
+        'number': '456987',
+        'from': 'Minsk',
+        'to': 'Warsaw',
+        'departure': 7-00,
+        'arrival': 8-00,
+  },
+  {
+        'number': '456321',
+        'from': 'Brest',
+        'to': 'Moscow',
+        'departure': 6-00,
+        'arrival': 8-30,
+  },
+  {
+        'number': '412397',
+        'from': 'Kiyv',
+        'to': 'Minsk,
+        'departure': 14-00,
+        'arrival': 23-30,
+  },
+]
+
+        for key, value in trains.items():
+            if 
+            print(f'{key}: {value}')
+        print('****')
