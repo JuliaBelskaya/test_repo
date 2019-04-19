aaf1acf6238b15946b9a9cc9f716e2409be8f70f
JuliaBelskaya <48747460+JuliaBelskaya@users.noreply.github.com>
2019-04-19 10:55:29 +0300

    add var2

--- HW09/hw09_tsk2.py
+++ HW09/hw09_tsk2.py
@@ -2,3 +2,8 @@
 # аргументов и выводит словарь с ключами удвоенной длины. {‘abc’: 5} -> {‘abcabc’: 5}
 
 print((lambda **kwargs: {k * 2: v for k, v in kwargs.items()})(abc=3, cde=6))
+
+#var2
+
+my_dict = {'a': 24, 'b': 33}
+print((lambda **kwargs: {key * 2: value for key, value in kwargs.items()})(**my_dict))
