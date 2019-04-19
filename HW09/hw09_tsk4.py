46755a80536b79de943e184f1b49b9d399f6f8de
JuliaBelskaya <48747460+JuliaBelskaya@users.noreply.github.com>
2019-04-19 10:56:46 +0300

    add var2

--- HW09/hw09_tsk4.py
+++ HW09/hw09_tsk4.py
@@ -13,3 +13,19 @@ def print_list(*args):
 
 
 print_list(1, 2, 3, 4)
+
+
+#var2
+
+def backwards_args(func):
+    def wrapper (*args, **kwargs):
+        return func(*args[::-1], **kwargs)
+    return wrapper
+
+
+@backwards_args
+def print_my_args(*args):
+   print(args)
+
+
+print_my_args(1, 2, 3, 4)
