60191e5e23b493ddfcb3031c1198f70a03a0e449
JuliaBelskaya <48747460+JuliaBelskaya@users.noreply.github.com>
2019-04-19 10:26:25 +0300

    add_variant2

--- HW04/task.py
+++ HW04/task.py
@@ -1,3 +1,7 @@
+# Дан список целых чисел. Создать новый список, каждый элемент которого 
+# равен исходному элементу умноженному на -2
+
+
 lst = list(range(1, 11))
 new_lst = []
 i = 0
@@ -6,3 +10,12 @@ while i < len(lst):
     i += 1
 print(new_lst)
 
+# вариант 2
+numbers = [1, 5, 7, -10, -2]
+mult_numbers = []
+i = 0
+numbers_len = len(numbers)
+while i < numbers_len:
+    mult_numbers.append(numbers[i] * -2)
+    i += 1
+print(mult_numbers)
