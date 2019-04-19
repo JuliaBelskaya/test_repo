865f811f4fe42134f930c18603bfc668900d5ec8
JuliaBelskaya <48747460+JuliaBelskaya@users.noreply.github.com>
2019-04-19 10:49:55 +0300

    add var2

--- HW06/task_8.py
+++ HW06/task_8.py
@@ -1,4 +1,15 @@
+# В заданной строке расположить в обратном порядке все слова.
+# Разделителями слов считаются пробелы. [02-7.2-HL08]
+
 string = input ('-->') 
 string = string.split(' ')
 string = string[::-1]
 print(string)
+
+#var2
+
+string = input('--> ')
+
+words = string.split(' ')
+new_string = ' '.join(words[::-1])
+print(new_string)
